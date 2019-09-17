a2b2337e99258597c81a653e1a1567772229179f
Lars Bek <38846235+NineNineFive@users.noreply.github.com>
2019-09-17 15:37:45 +0200

    Initializing Project with JavaFX Files

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,8 +1,23 @@
 package Files;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
 
+// Module View Controller
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("test");
+        launch(args);
+    }
+
+    @Override public void start(Stage stage){
+        // Initialize Application Scene
+        stage.setTitle("Pathfinding Project");
+        Group group = new Group();
+        stage.setScene(new Scene(group,400,300));
+        stage.setMaximized(true);
+        stage.show();
     }
 }
