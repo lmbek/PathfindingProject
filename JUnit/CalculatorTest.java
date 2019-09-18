b4dd9332c6ccff27031c54741176bcc109006342
Lars Bek <38846235+NineNineFive@users.noreply.github.com>
2019-09-18 10:18:04 +0200

    adding JUnit Testing to project

---
+++ JUnit/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+public class CalculatorTest {
+    @Test
+    public void TestingAdd(){
+        System.out.println("Hej");
+        assertTrue(false);
+    }
+
+}
---
+++ src/Files/Calculator.java
@@ -0,0 +1,7 @@
+package Files;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a+b;
+    }
+}
