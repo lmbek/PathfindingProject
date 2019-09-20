89f17f2659c74ecf90f8f18d19c1dbf30720d1f1
JonatanRytter <JonatanRytter@users.noreply.github.com>
2019-09-20 13:11:52 +0200

    Added MyCanvas class

--- src/Files/Views/MyCanvas.java
+++ src/Files/Views/MyCanvas.java
@@ -1,4 +1,19 @@
 package Files.Views;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 public class MyCanvas {
+
+    public MyCanvas(){
+        Canvas canvas = new Canvas();
+        canvas.setHeight(300);
+        canvas.setWidth(300);
+        Rectangle rectangle = new Rectangle(100, 100, 100, 100);
+        Text text = new Text();
+        text.setText("hi");
+        canvas.getGraphicsContext2D();
+    }
 }
