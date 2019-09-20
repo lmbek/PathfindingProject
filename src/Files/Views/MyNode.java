727e2711b580c7770883e3b83f31b4684c36fece
shazidy <shazidy@gmail.com>
2019-09-20 12:48:13 +0200

    commit my canvas

---
+++ src/Files/Views/MyNode.java
@@ -0,0 +1,14 @@
+package Files.Views;
+
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+public class MyNode extends Parent {
+
+    public void addNode(Node child) {
+
+        this.getChildren().add(child);
+
+
+    }
+}
