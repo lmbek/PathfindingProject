4a53026c9a6bcdf90d0365c2ae56f3667d6e1cb3
Lars Bek <38846235+NineNineFive@users.noreply.github.com>
2019-10-11 11:01:29 +0200

    Added Circle, Line and Text. Removed pane.setStyle

---
+++ src/files/views/environment/Line.java
@@ -0,0 +1,34 @@
+package files.views.environment;
+
+import files.views.Environment;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/*************
+ **************  MADE BY LARS MORTEN BEK | Miniproject | Mastermind
+ *************/
+
+
+public class Line implements Environment {
+    public int x,y,dx,dy;
+
+    public Line(int x,int y,int dx, int dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.setStroke(Color.BLACK);
+        gc.beginPath();
+        gc.moveTo(x,y);
+        gc.lineTo(dx,dy);
+        gc.stroke();
+
+    }
+
+
+}
