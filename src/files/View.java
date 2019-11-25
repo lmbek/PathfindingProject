534d9d06615ee2a5f467a0ccf8a642210a9a73a0
NineNineFive <38846235+NineNineFive@users.noreply.github.com>
2019-11-25 03:58:44 +0100

    committing very nice, responsive and resizable GUI And Restructuring of project

--- src/files/View.java
+++ src/files/View.java
@@ -6,32 +6,26 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 // View manages the graphics
 // This is the Root node of Application
-public class View extends Group {
+public class View {
     private static boolean created = false; // if created
-    private UserInterface UI;
     private Stage stage;
 
     // Settings
     private int sceneWidth = 1280;
     private int sceneHeight = 720;
     private String viewFXMLLink = "views/view.fxml";
-    private String viewCSSLink = "views/view.css";
-    private Node fxml;
-
+    private UserInterface UI;
 
     View(Stage stage){
         this.onlyOneInstance(); // Limit to only 1 instance of the Model class
         this.stage = stage;
-        this.loadFXML();
-        //this.loadCSS();
-        UI = new UserInterface(this);
-        this.getChildren().add(UI);
-        addUI();
+        Node fxml = this.loadFXML();
+        this.setScene((Parent)fxml);
+        this.UI = new UserInterface(fxml);
     }
 
     private void onlyOneInstance (){
@@ -44,58 +38,24 @@ public class View extends Group {
         }
     }
 
-    private void loadFXML(){
+    private Node loadFXML(){
         try {
-            System.out.println("load");
-            this.fxml = FXMLLoader.load(getClass().getResource(this.viewFXMLLink));
-            this.setScene(stage);
-            this.convertFXMLToNodes();
+            return FXMLLoader.load(getClass().getResource(this.viewFXMLLink));
         } catch (Exception exception){
-            System.err.println(exception);
-            System.err.println("FXML couldnt load...");
+            System.err.println("FXML couldnt load..."); // dont care about the exception message, just tell us fxml didnt load
             System.exit(0);
-        }
-    }
-/*
-    private void loadCSS(){
-        try {
-            this.css = FXMLLoader.load(getClass().getResource(this.viewCSSLink));
-            this.convertFXMLToNodes();
-        } catch (Exception exception){
-            System.err.println(exception);
-            System.err.println("CSS couldnt load...");
-            System.exit(0);
+            return null;
         }
     }
 
- */
-
-    private void convertFXMLToNodes(){
-        try {
-            /*
-            selectFrom = (ComboBox) this.fxml.lookup("#selectFrom");
-            button = (Button) this.fxml.lookup("#seeDepartures");
-            tableView = (TableView) this.fxml.lookup("#tableView");
-            message = (Label) this.fxml.lookup("#message");
-            */
-            System.out.println("test");
-        } catch(Exception exception){
-            System.err.println(exception);
-            System.err.println("Couldnt convert FXML to Nodes. Check if Type is correct.");
-            System.exit(0);
-        }
+    private void setScene(Parent fxml){
+        this.stage.setScene(new Scene(fxml, sceneWidth, sceneHeight)); // Scene with ScrollPane
     }
-
-    private void setScene(Stage stage){
-        stage.setScene(new Scene((Parent)this.fxml, sceneWidth, sceneHeight)); // Scene with ScrollPane
-    }
-
 
     public UserInterface getUI(){
         return UI;
     }
 
-    private void addUI(){ }
     public void setMaximized(Boolean bool){
         this.stage.setMaximized(bool);
     }
