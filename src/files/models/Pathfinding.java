19f090bec724f38037cce0fb86c4e47e635b8263
NineNineFive <38846235+NineNineFive@users.noreply.github.com>
2019-11-25 08:03:44 +0100

    committing dijkstra

--- src/files/models/Pathfinding.java
+++ src/files/models/Pathfinding.java
@@ -1,103 +1,114 @@
 package files.models;
 
+import files.models.graph.Edge;
+import files.models.graph.Vertex;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Pathfinding {
 
-    public static void main(String[] args) {
-        Pathfinding pathfinding = new Pathfinding();
-        pathfinding.startDijkstra();
-    }
 
-    public void startDijkstra(){
-        // Create graph
-        Graph graph = this.makeSmallGraphB();
-        // A
-        //Vertex startNode = graph.getVertex("A");
-        //Vertex endNode = graph.getVertex("E");
+/*
+    int infinity = (int)Double.POSITIVE_INFINITY;
 
-        // B
-        Vertex startNode = graph.getVertex("J");
-        Vertex endNode = graph.getVertex("F");
-
-        Pair<Integer, Map<Vertex, Vertex>> results=graph.dijkstra(startNode, endNode);
-        Vertex current =endNode;
-        ArrayList<Vertex> Path= new ArrayList<>();
-        Path.add(endNode);
-
-        while ((current != startNode) && (results.getValue().get(current)!=null)) {
-            current=results.getValue().get(current);
-            Path.add(0,current);
+    public Pair<Integer, Map<Vertex,Vertex>> oldDijkstra(Vertex startNode, Vertex endNode)
+    {
+        Map<Vertex,Vertex> predecessorMap= new HashMap<>(); // The key is a vertex, the value is the previous vertex
+        Map<Vertex,Integer> distanceMap=new HashMap<>(); // The key is a vertex, the value is the shortest distance to it
+        // initialize arrays
+        for(Vertex vertex: graph.getVertices()) {
+            distanceMap.put(vertex,infinity); // This is the nodes and their weight
+            predecessorMap.put(vertex, null); // we set the vertex to be null, which is meant to be "not visited"
         }
 
-        for(Vertex v : Path)
-        {
-            System.out.print( v.name);
-            if (v!=endNode)
-                System.out.print("->");
+        /////////////////////////////implement Dijkstra /////////////////////////////
+        // Initialize values
+
+
+        //Vertex previous = new Vertex("Start"); // Set start to previous
+
+        Vertex previous = startNode; // Set previous to be start node
+        Vertex current = null;
+        Vertex next = null;
+
+        distanceMap.put(previous,0); // Setting previous distance map to 0
+        predecessorMap.put(startNode,new Vertex("Start")); // Initialize the start as "coming from previous" (which at start is start)
+
+        ArrayList<Vertex> queue = new ArrayList<Vertex>();
+
+        int minimum = infinity;
+        int weight = infinity;
+
+        while(predecessorMap.containsValue(null)){
+            //TODO: what would happen if we start at a point, where we cant evaluate everything?
+            // TODO: FIX QUEUE, Make a foreach node check after evaluating every network from the startNode
+            // TODO: print a message if the system couldnt find endNode, fix the remove script
+            System.out.println(previous.name+" is having the weight of: "+distanceMap.get(previous));
+            System.out.println("For: " + previous.name + " {");
+
+            for (Edge edge : previous.getEdges()) {
+                if (!previous.equals(edge.getToVertex())) {
+                    current = edge.getToVertex(); // Current is set to the end of the edge, if the relation arrow is pointing outwards
+                    if (predecessorMap.get(current) == null) { // check if the end of the edge is null (available)
+                        System.out.print("\t From: " + previous.name + " "); System.out.print("\t To: " + current.name + " ");
+                        System.out.print("\t Distance: " + edge.distance); System.out.println();
+
+                        if (weight <= distanceMap.get(current)) {
+                            weight = edge.distance+distanceMap.get(previous);
+                            distanceMap.put(current, weight); // Set the weight to distMap
+                            System.out.println("\t " + current.name + " is now: " + weight);
+                        } else {
+                            predecessorMap.remove(previous); // Removing vertex
+                            System.out.println("\t removed " + previous.name);
+                            // we are not setting a new previous here, the reason is that we instead had a loop that runs from the other vertices.
+                        }
+
+                        if (minimum >= edge.distance) {
+                            minimum = edge.distance; // Setting the minimum to be the lowest of the edge distances in this for loop
+                            next = current;
+                        } else if(predecessorMap.get(current)==null&&!current.equals(endNode)) {
+                            queue.add(current); // This is the Queue of the other nodes, that is higher than the minimum
+                        }
+                    } else {
+                        System.out.println("not evaluating: " + current.name);
+                    }
+                }
+            }
+
+            System.out.println("Queue size: "+queue.size());
+            for(Vertex q : queue){
+                System.out.println("In Queue-> "+q.name);
+            }
+
+
+            if(previous==next&&!next.equals(endNode)){
+
+                System.out.println("We jump (queue)");
+
+                next = queue.get(0);
+
+                System.out.println("Jumper: i put key: "+next.name+" the value: "+predecessorMap.get(previous).name);
+                predecessorMap.put(next,predecessorMap.get(previous));
+                current = queue.get(0);
+                previous = queue.get(0);
+                queue.remove(0);
+
+            } else {
+                System.out.println("i put key: "+next.name+" the value: "+previous.name);
+                predecessorMap.put(next,previous);
+                previous = next;
+                System.out.println("\t Minimum is: " + minimum + " So we select: " + next.name);
+                System.out.println("}");
+                System.out.println();
+
+            }
         }
 
+        return (new Pair<Integer,Map<Vertex,Vertex>> (distanceMap.get(startNode), predecessorMap));
     }
-
-    public Graph makeSmallGraphA() {
-        Graph myGraph= new Graph();
-        final Vertex A= myGraph.addVertex("A");
-        final Vertex B= myGraph.addVertex("B");
-        final Vertex C = myGraph.addVertex("C");
-        final Vertex D = myGraph.addVertex("D");
-        final Vertex E = myGraph.addVertex("E");
-
-        myGraph.newEdge(A,B, 5,  3);
-        myGraph.newEdge(A,C, 10,  3);
-        myGraph.newEdge(B,C, 3,  3);
-        myGraph.newEdge(B,D, 2,  3);
-        myGraph.newEdge(B,E, 9,  3);
-        myGraph.newEdge(C,B, 2,  3);
-        myGraph.newEdge(C,E, 1,  3);
-        myGraph.newEdge(D,E, 6,  3);
-        myGraph.newEdge(E,D, 4,  3);
-
-        return myGraph;
-    }
-
-    public Graph makeSmallGraphB() {
-        Graph myGraph= new Graph();
-        final Vertex A = myGraph.addVertex("A");
-        final Vertex B = myGraph.addVertex("B");
-        final Vertex C = myGraph.addVertex("C");
-        final Vertex D = myGraph.addVertex("D");
-        final Vertex E = myGraph.addVertex("E");
-        final Vertex F = myGraph.addVertex("F");
-        final Vertex G = myGraph.addVertex("G");
-        final Vertex H = myGraph.addVertex("H");
-        final Vertex I = myGraph.addVertex("I");
-        final Vertex J = myGraph.addVertex("J");
-
-        myGraph.newEdge(A,B,10,0);
-        myGraph.newEdge(A,D,20,0);
-        myGraph.newEdge(A,E,20,0);
-        myGraph.newEdge(A,F,5,0);
-        myGraph.newEdge(A,G,15,0);
-        myGraph.newEdge(B,C,7,0);
-        myGraph.newEdge(B,D,10,0);
-        myGraph.newEdge(C,B,15,0);
-        myGraph.newEdge(C,D,5,0);
-        myGraph.newEdge(D,E,10,0);
-        myGraph.newEdge(E,F,5,0);
-        myGraph.newEdge(G,F,10,0);
-        myGraph.newEdge(H,A,5,0);
-        myGraph.newEdge(H,B,20,0);
-        myGraph.newEdge(H,G,5,0);
-        myGraph.newEdge(I,B,15,0);
-        myGraph.newEdge(I,H,20,0);
-        myGraph.newEdge(I,J,10,0);
-        myGraph.newEdge(J,B,5,0);
-        myGraph.newEdge(J,C,15,0);
-
-        return myGraph;
-    }
+*/
 
 }
